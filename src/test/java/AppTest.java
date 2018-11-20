aeabc436920616390775e2012a62339a511ae757
CSV-Tom <tom.buhrtz@gmail.com>
2018-11-20 19:38:45 +0100

    Add junit plugin, eclipse plugin and application plugin

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -7,6 +7,6 @@ public class AppTest {
     @Test public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-        assertNull("app should have a greeting", classUnderTest.getGreeting());
+        // assertNull("app should have a greeting", classUnderTest.getGreeting());
     }
 }
